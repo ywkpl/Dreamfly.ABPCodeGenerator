2c437e10fe2ba1f6c06d28e3ed9fa57e79532a7d
ywkpl <ywkpl@hotmail.com>
2020-05-11 23:44:24 +0800

    init

---
+++ Dreamfly.ABPCodeGenerator/Models/Project.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Dreamfly.ABPCodeGenerator.Models
+{
+    public class Project
+    {
+        public string EmailAddress { get; set; }
+        public string Author { get; set; }
+        public string OutputPath { get; set; }
+        public string Version { get; set; }
+        public string Entity { get; set; }
+        public string Module { get; set; }
+        public string Name { get; set; }
+        public BuildTask BuildTasks { get; set; }
+    }
+
+    public class BuildTask
+    {
+        public List<string> ABPProjectNames { get; set; }
+        public Template[] Templates { get; set; }
+    }
+
+    public class Template
+    {
+        public string File { get; set; }
+        public string Remark { get; set; }
+        public bool? IsExecute { get; set; }
+        public Output Output { get; set; }
+    }
+
+    public class Output
+    {
+        public string Folder { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Dreamfly.ABPCodeGenerator/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Dreamfly.ABPCodeGenerator.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Dreamfly.ABPCodeGenerator
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOptions();
+            services.Configure<Project>(Configuration.GetSection("Project"));
+
+            IFileProvider fileProvider=new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Templates"));
+            services.AddSingleton<IFileProvider>(fileProvider);
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
