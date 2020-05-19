451afcf0164d1ad6347cd757a6589a2b520149af
ywkpl <ywkpl@hotmail.com>
2020-05-19 17:58:07 +0800

    feat:add react umi ui

---
+++ Dreamfly.ABPCodeGenerator.UI/src/pages/Setting/Project/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+
+const Project: React.FC<{}> = () => {
+  return (
+    <PageHeaderWrapper>
+      <div>test11</div>
+    </PageHeaderWrapper>
+  );
+};
+
+export default Project;
