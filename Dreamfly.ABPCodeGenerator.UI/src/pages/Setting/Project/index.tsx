fbbe42f0ef8cfc0266f23b488a27a6f7211ef5d1
ywkpl <ywkpl@hotmail.com>
2020-05-31 22:39:13 +0800

    feat:change project page: add create model, items

--- Dreamfly.ABPCodeGenerator.UI/src/pages/Setting/Project/index.tsx
+++ Dreamfly.ABPCodeGenerator.UI/src/pages/Setting/Project/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Form, Card, Input, Button, Table, Divider, Space, Switch, Checkbox } from 'antd';
+import { Form, Card, Input, Button, Table, Divider, Space, Switch, Checkbox, Modal } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { useDispatch, useSelector, Loading } from 'umi';
 import { ProjectType, ProjectTemplate } from './model';
@@ -10,14 +10,15 @@ const FormItem = Form.Item;
 
 interface ProjectPageState {
   loading: Loading;
-  project: ProjectType;
+  project: { project: ProjectType; editModelVisible: boolean };
 }
 
 const Project = () => {
   const [form] = Form.useForm();
   const dispatch = useDispatch();
-  const { project, submitting } = useSelector((state: ProjectPageState) => ({
-    project: state.project,
+  const { project, editModelVisible, submitting } = useSelector((state: ProjectPageState) => ({
+    project: state.project.project,
+    editModelVisible: state.project.editModelVisible,
     submitting: state.loading.effects['project/updateProject'],
   }));
 
@@ -30,6 +31,15 @@ const Project = () => {
       xs: { span: 24 },
       sm: { span: 12 },
       md: { span: 10 },
+    },
+  };
+
+  const formAllItemLayout = {
+    labelCol: {
+      span: 5,
+    },
+    wrapperCol: {
+      span: 17,
     },
   };
 
@@ -51,41 +61,15 @@ const Project = () => {
       align: 'center',
       render: (value, row) => {
         return (
-          <>
-            <Checkbox
-              checked={value}
-              value={row.file}
-              onChange={(v) => {
-                const { templates } = project;
-                const template = templates?.find((t: ProjectTemplate) => t.file === v.target.value);
-                if (template) {
-                  template.isExecute = v.target.checked;
-                }
-                console.log(v);
-                console.log(template);
-              }}
-            />
-            <Switch
-              checked={value}
-              onChange={(v) => {
-                dispatch({
-                  type: 'project/test',
-                  payload: {
-                    ...project,
-                    name: 'test',
-                  },
-                });
-                const { templates } = project;
-                const template = templates?.find((t) => t.file === row.file);
-                if (template) {
-                  console.log('test');
-                  template.isExecute = v;
-                }
-                console.log(v);
-                console.log(row);
-              }}
-            />
-          </>
+          <Switch
+            checked={value}
+            onChange={(v) => {
+              dispatch({
+                type: 'project/changeTemplateExecute',
+                payload: { file: row.file, isExecute: v },
+              });
+            }}
+          />
         );
       },
     },
@@ -118,26 +102,124 @@ const Project = () => {
 
   const handleAdd = () => {
     dispatch({
-      type: 'project/addTemplate',
+      type: 'project/saveEditModelVisible',
+      payload: true,
+    });
+  };
+
+  const handleSave = () => {
+    form.validateFields().then((values) => {
+      console.log({
+        ...values,
+        templates: project.templates,
+      });
+      // dispatch({
+      //   type: 'project/updateProject',
+      //   payload: {
+      //     ...project,
+      //     ...values,
+      //   },
+      // });
     });
   };
 
   const saveButton = (
-    <Button
-      type="primary"
-      htmlType="submit"
-      onClick={() => {
-        form.validateFields().then((values) => {
-          // console.log(submitting)
-          // setTimeout(() => {}, 5000);
-          console.log(values);
-          //dispatch({ type: 'project/updateProject', payload: values });
-        });
-      }}
-      loading={submitting}
-    >
+    <Button type="primary" htmlType="submit" onClick={handleSave} loading={submitting}>
       保存
     </Button>
+  );
+
+  const handleEditModelOk = () => {};
+  const handleEditModelCancel = () => {
+    dispatch({
+      type: 'project/saveEditModelVisible',
+      payload: false,
+    });
+  };
+
+  const modelEdit = (
+    <Modal
+      title="模板明细"
+      destroyOnClose
+      visible={editModelVisible}
+      onOk={handleEditModelOk}
+      okText="保存"
+      onCancel={handleEditModelCancel}
+    >
+      <Form style={{ marginTop: 8 }} form={form} initialValues={project}>
+        <FormItem
+          {...formAllItemLayout}
+          label="模板路径"
+          name="file"
+          rules={[
+            {
+              required: true,
+              message: '请输入模板路径',
+            },
+          ]}
+        >
+          <Input placeholder="模板路径" />
+        </FormItem>
+        <FormItem
+          {...formAllItemLayout}
+          label="说明"
+          name="remark"
+          rules={[
+            {
+              required: true,
+              message: '请输入说明',
+            },
+          ]}
+        >
+          <Input placeholder="说明" />
+        </FormItem>
+
+        <FormItem {...formAllItemLayout} label="是否生成" name="isExecute" valuePropName="checked">
+          <Switch />
+        </FormItem>
+
+        <FormItem
+          {...formAllItemLayout}
+          label="生成目录"
+          name="outputFolder"
+          rules={[
+            {
+              required: true,
+              message: '请输入生成目录',
+            },
+          ]}
+        >
+          <Input placeholder="生成目录" />
+        </FormItem>
+
+        <FormItem
+          {...formAllItemLayout}
+          label="生成文件名"
+          name="outputName"
+          rules={[
+            {
+              required: true,
+              message: '请输入生成文件名',
+            },
+          ]}
+        >
+          <Input placeholder="生成文件名" />
+        </FormItem>
+        <FormItem
+          {...formAllItemLayout}
+          label="项目文件"
+          name="projectFile"
+          rules={[
+            {
+              required: true,
+              message: '请输入项目文件',
+            },
+          ]}
+        >
+          <Input placeholder="项目文件" />
+        </FormItem>
+      </Form>
+    </Modal>
   );
 
   return (
@@ -183,11 +265,6 @@ const Project = () => {
               <FormItem {...formItemLayout} label="说明" name={['author', 'remark']}>
                 <Input placeholder="说明" />
               </FormItem>
-              {/* <FormItem {...submitFormLayout} style={{ marginTop: 32 }}>
-                <Button type="primary" htmlType="submit" loading={submitting}>
-                  保存
-                </Button>
-              </FormItem> */}
             </Card>
             <Card bordered={false} title="模板">
               <div className={styles.tableList}>
@@ -195,7 +272,6 @@ const Project = () => {
                   <Button icon="plus" type="primary" onClick={handleAdd}>
                     新建
                   </Button>
-
                   <Button icon="danger" type="primary" onClick={handleAdd}>
                     刪除
                   </Button>
@@ -214,6 +290,7 @@ const Project = () => {
           </Space>
         </Form>
       )}
+      {editModelVisible && modelEdit}
     </PageHeaderWrapper>
   );
 };
---
+++ Dreamfly.ABPCodeGenerator.UI/src/pages/Setting/Project/model.tsx
@@ -0,0 +1,94 @@
+import { Effect, Reducer } from 'umi';
+import { message } from 'antd';
+import { getProject, updateProject } from './service';
+
+const Model: ModelType = {
+  namespace: 'project',
+  state: {
+    project: {},
+    editModelVisible: false,
+  },
+  effects: {
+    *updateProject({ payload }, { call }) {
+      yield call(updateProject, payload);
+      message.success('保存成功！');
+    },
+    *getProject({ payload }, { call, put }) {
+      const response = yield call(getProject, payload);
+      yield put({
+        type: 'save',
+        payload: response,
+      });
+    },
+    *changeTemplateExecute({ payload }, { put, select }) {
+      const { project } = yield select((state: ProjectStateType) => state.project);
+      const template = project.templates?.find((t: ProjectTemplate) => t.file === payload.file);
+      if (template) {
+        template.isExecute = payload.isExecute;
+        yield put({
+          type: 'save',
+          payload: project,
+        });
+      }
+    },
+  },
+  reducers: {
+    saveEditModelVisible(state, action) {
+      return {
+        ...state,
+        editModelVisible: action.payload,
+      };
+    },
+    save(state, action) {
+      return {
+        ...state,
+        project: action.payload as ProjectType,
+      };
+    },
+  },
+};
+
+//#region Interfaces
+export interface AuthorType {
+  name?: string;
+  email?: string;
+  remark?: string;
+}
+
+export interface ProjectTemplate {
+  file: string;
+  remark: string;
+  isExecute: boolean;
+  outputFolder: string;
+  outputName: string;
+  projectFile: string;
+}
+
+export interface ProjectType {
+  name?: string;
+  author?: AuthorType;
+  version?: string;
+  templates?: Array<ProjectTemplate>;
+}
+
+export interface ProjectStateType {
+  project?: ProjectType;
+  editModelVisible?: boolean;
+}
+
+export interface ModelType {
+  namespace: string;
+  state: ProjectStateType;
+  effects: {
+    updateProject: Effect;
+    getProject: Effect;
+    changeTemplateExecute: Effect;
+  };
+  reducers: {
+    saveEditModelVisible: Reducer<ProjectStateType>;
+    save: Reducer<ProjectStateType>;
+  };
+}
+//#endregion
+
+export default Model;
