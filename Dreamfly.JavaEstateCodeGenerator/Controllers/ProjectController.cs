44c532da56163297d942358c1321331d74bf521f
ywkpl <ywkpl@hotmail.com>
2021-04-21 15:54:36 +0800

    feat:change field settings

--- Dreamfly.JavaEstateCodeGenerator/Controllers/ProjectController.cs
+++ Dreamfly.JavaEstateCodeGenerator/Controllers/ProjectController.cs
@@ -48,6 +48,8 @@ namespace Dreamfly.JavaEstateCodeGenerator.Controllers
             new CodeSql(_project).UpdateSqliteCodeKeyValues();
         }
 
+
+
 //        [HttpPost("ToDb")]
 //        public void SaveJsonToDb()
 //        {
