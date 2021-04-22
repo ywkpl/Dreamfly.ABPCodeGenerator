bf36ce3855c753252689ba3a0e4f67c23a8c8527
ywkpl <ywkpl@hotmail.com>
2021-04-22 17:54:02 +0800

    feat:change setting

---
+++ Dreamfly.JavaEstateCodeGenerator/Core/InitilizeSql.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Dreamfly.JavaEstateCodeGenerator.Models;
+
+namespace Dreamfly.JavaEstateCodeGenerator.Core
+{
+    public class InitilizeSql
+    {
+        private readonly Project _project;
+        private readonly String filePath;
+        public InitilizeSql(Project project)
+        {
+            this._project = project;
+            filePath = Path.Combine(_project.OutputPath,
+                "..\\..\\baseinfo-service\\src\\main\\resources\\Initilize.sql");
+
+        }
+
+        public static void GeneratorCheckSetting()
+        {
+            int startId = 10, detailStartId=10;
+
+        }
+    }
+}
